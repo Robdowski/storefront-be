64d764297422fa37e6d2cea4521f60eda5b00e21
Robert Carter <rc3331@gmail.com>
2020-02-23 18:39:36 -0600

    fixed spelling error in schema builder

--- migrations/20200220182828_user_order.js
+++ migrations/20200220182828_user_order.js
@@ -2,7 +2,7 @@
 exports.up = function(knex) {
     return knex.schema.createTable('user_order', tbl => {
         tbl.increments()
-        tbl.integer('user_id').references('id').inTable('user')
+        tbl.integer('user_id').references('id').inTable('users')
     })
   
 };
