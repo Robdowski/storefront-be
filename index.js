73e2a26c5432eb07454a981935bd887ab2d764ea
Robert Carter <rc3331@gmail.com>
2020-02-23 19:15:15 -0600

    heroku postgresql now setup and accepting requests, added / route that notifies server is up

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const server = require("./server");
 const port = process.env.PORT || 4000;
 
 
+server.get('/', (req, res) => {
+  res.status(200).json({"Message": "Server is up!"})
+})
+
+
 server.listen(port, () => {
   console.log(`\n** Running on port ${port} **\n`);
 });
